9c810bd70e9c1ac3d512b45747934641e4805276
marc zarb <marc.zarb91@gmail.com>
2019-05-25 16:38:21 +0200

    front dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-
   def dashboard
     @spaces = current_user.spaces
     skip_authorization
+  end
+
+  def destroy
+    @space = Space.find(current_user.space[:id])
+    @space.destroy
   end
 end

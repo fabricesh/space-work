7d3fafec7b72d964ef310312c26751413a2e841a
Nicolas Leguillon <legnico75@gmail.com>
2019-05-26 21:33:08 +0200

    map online

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
 //= require rails-ujs
 //= require_tree .
-//= require bootstrap-datepicker
-import flatpickr from 'flatpickr'
-import 'flatpickr/dist/flatpickr.min.css'
 
-
-flatpickr(".datepicker", {
-
-});
--- app/models/space.rb
+++ app/models/space.rb
@@ -6,4 +6,6 @@ class Space < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   validates :title, :localisation, :availabilities, :price, :space_type, :capacity, presence: true
 
+  geocoded_by :localisation
+  after_validation :geocode, if: :will_save_change_to_localisation?
 end
